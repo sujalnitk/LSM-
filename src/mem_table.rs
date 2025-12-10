50842be56677fff7f14ad1bf39f2b46b5f8df42a
GhostBYte <sksujaliitg@gmail.com>
2025-12-10 19:27:12 +0530

    defined create new MemTable fn

--- src/mem_table.rs
+++ src/mem_table.rs
@@ -1,9 +1,28 @@
 use std::collections::Hashmap;
 use std::fs::File;
+use std::io::Write;
 
 pub struct MemTable {
-    map: Hashmap,
+    map: Hashmap<Vec<u8>, Vec<u8>>,
     WAL: Option<File>,
     id: usize,
     size: usize,
+}
+
+impl MemTable {
+    pub fn new(id: usize, size: usize) -> Self {
+        let wal_file = format!("wal_{}.log", id);
+        let file = File::create(&wal_file).expect("Could not create wal file");
+
+        Self {
+            map: Hashmap::new(),
+            WAL: Some(file),
+            id,
+            size,
+        }
+    }
+}
+
+fn main {
+    let table = MemTable::new();
 }
